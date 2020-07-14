c2d14b0349030437060ecbcd86c336627174bfef
EricksonLopes <ofc.erickson@gmail.com>
2020-07-14 09:29:29 -0300

    modeling class categories and product

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from portal.models import Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    # populado automaticamente
+    prepopulated_fields = {
+        # meu campo slug quando for chamado vai ser populado baseado no name
+        "slug":('name', )
+    }
+    # filtrar aos que estão escondido e não escondido
+    list_filter = ['hidden']
+
+    # quais itens trazer
+    list_display = ('id', 'name', 'parent', 'hidden')
+
+
+# registra o model e usa as configurações da classe indicada
+admin.site.register(Category, CategoryAdmin)
--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Moledo de base das categorias do site
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    # URL
+    slug = models.SlugField(unique=True)
+    # chave primaria das categorias das categorias
+    parent = models.ForeignKey('Category', on_delete=models.CASCADE,null=True, blank=True, related_name='cat_child' )
+    # ordem que ira aparecer
+    order = models.IntegerField (blank=True, null=True)
+    # se vai ser visivel
+    hidden = models.BooleanField(default=False)
+
+    class Meta:
+        # Configura manualmente o nome em plural
+        verbose_name_plural = 'Caregories'
+
+    # transfoma em string do nome do objeto
+    def __str__(self):
+        return self.name
+
+
+# Modelo de bado dos produtos]
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    # apontamento para apenas um dono / usuario logado na hora do registro
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    # Relacionando categorias ao produto / Um produto pode ter diversas categorias
+    categories = models.ManyToManyField(Category, blank=True, related_name='categories')
+    # quantidade do produto
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    short_description = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    # Status do produto
+    STATUS_CHOICES = (
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Inactive')
+
+    class Meta:
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.name
