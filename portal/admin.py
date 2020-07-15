3ca4b68a53dcb97e897747367b7993dab3ade72b
EricksonLopes <ofc.erickson@gmail.com>
2020-07-15 09:37:48 -0300

    s

--- portal/admin.py
+++ portal/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from portal.models import Category, Product
+from portal.models import *
 
 class CategoryAdmin(admin.ModelAdmin):
     # populado automaticamente
@@ -26,8 +26,22 @@ class ProductAdmin(admin.ModelAdmin):
     # quais itens trazer
     list_display = ('id', 'name', 'short_description', 'status')
 
-# registra o model e usa as configurações da classe indicada
-admin.site.register(Category, CategoryAdmin)
+# juntar tbm as perguntas referentes as respostas
+class ProductAnserInline(admin.StackedInline):
+    model = ProductAnswers
+    # se pode deletar
+    can_delete = False
+
+class ProductQuestionsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'question', 'status')
+
+    # toda vez que tiver uma pergunta as respostas tem que aparecer de forma organizada
+    inlines = (ProductAnserInline, )
+
+
 
 # registra o model e usa as configurações da classe indicada
+admin.site.register(Category, CategoryAdmin)
+# registra o model e usa as configurações da classe indicada
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductQuestions, ProductQuestionsAdmin)
